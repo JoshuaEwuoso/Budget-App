ad8e803b4ec8434b12ea95c9e0536e1cc2128e56
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-19 12:46:11 -0500

    adds put, post, delete

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const transactions = express.Router();
-const transactionArray = require("../models/transactions");
+const transacArray = require("../models/transactions");
 
 transactions.get("/", (request, response) => {
     response.json(transactionArray);
@@ -8,12 +8,30 @@ transactions.get("/", (request, response) => {
 
 transactions.get("/:id", (request, response) => {
     const { id } = request.params;
-    if(transactionArray[id]){
-        response.send(transactionArray[id]);
+    if(transacArray[id]){
+        response.json(transacArray[id]);
     } else {
         response.redirect("*");
     }
 });
 
+transactions.put("/:id", async (request, response) => {
+    const { id } = request.params;
+    transacArray[id] = request.body;
+    response.status(200).json(transacArray[id]);
+  });
+  
+  
+transactions.post("/", (request, response) => {
+    const updatedArray = transacArray.push(request.body);
+    response.json(transacArray[updatedArray - 1]);
+  });
+  
+
+transactions.delete("/:id", (request, response) => {
+    const { id } = request.params;
+    const deletedTransac = transacArray.splice(id, 1);
+    response.status(200).json(deletedTransac);
+  });
 
 module.exports = transactions;
